1e83a4ca3e14250f02421c87750d6f890e7da4b4
Dave Snider <dave.snider@gmail.com>
2023-06-29 10:02:36 -0400

    back to previous format

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['eslint:recommended', 'prettier', 'plugin:mdx/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2015
+  },
+  settings: {
+    'mdx/code-blocks': true,
+    'mdx/language-mapper': {}
+  },
+  ignorePatterns: ['node_modules/', 'package.json', 'pnpm-lock.yaml'],
+  rules: {
+    'react/jsx-no-undef': 'off'
+  }
+};
