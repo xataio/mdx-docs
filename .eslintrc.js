a867114e722e199eeeb867d0a143131b9dc729fa
Dave Snider <dave.snider@gmail.com>
2023-06-29 09:44:29 -0400

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
-module.exports = {
-  extends: ['eslint:recommended', 'prettier', 'plugin:mdx/recommended', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 2015
-  },
-  settings: {
-    'mdx/code-blocks': true,
-    'mdx/language-mapper': {}
-  },
-  rules: {
-    'react/jsx-no-undef': 'off'
+export default [
+  {
+    extends: ['eslint:recommended', 'prettier', 'plugin:mdx/recommended', 'plugin:prettier/recommended'],
+    parserOptions: {
+      ecmaVersion: 2015
+    },
+    settings: {
+      'mdx/code-blocks': true,
+      'mdx/language-mapper': {}
+    },
+    ignorePatterns: ['node_modules/', 'package.json', 'pnpm-lock.yaml'],
+    rules: {
+      'react/jsx-no-undef': 'off'
+    }
   }
-};
+];
